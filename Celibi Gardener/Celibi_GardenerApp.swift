aa03b1f451162c4721e9ee0bdfcc0fe41b3c6c2a
Andrew Addis <addisandrew1@gmail.com>
2021-11-18 18:07:08 +0900

    Initial Commit

---
+++ Celibi Gardener/Celibi_GardenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Celibi_GardenerApp.swift
+//  Celibi Gardener
+//
+//  Created by Andrew Addis on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct Celibi_GardenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
